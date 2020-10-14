967a1ca34024d70b9d6008286f9e9eea88355b03
Aparna Dutta <aparnad198@gmail.com>
2020-10-14 10:58:49 -0400

    Changed raw counts to probability distribution

--- wheel_of_fortune.py
+++ wheel_of_fortune.py
@@ -1,5 +1,5 @@
-from collections import Counter
-from typing import List
+from collections import Counter, defaultdict
+from typing import List, DefaultDict
 
 categories = ["around_the_house.txt",
               "before_and_after.txt",
@@ -9,8 +9,10 @@ categories = ["around_the_house.txt",
               "college_life.txt",
               "event.txt"]
 
+
 def count_chars(path: str):
     counts: Counter[str] = Counter()
+    dists: DefaultDict = defaultdict()
     category = path[:-4].replace("_", " ").title()
     num_entries = 0
     with open(path, encoding="utf8") as file:
@@ -18,14 +20,18 @@ def count_chars(path: str):
             num_entries += 1
             phrase = line.replace(" ", "").rstrip("\n")
             counts.update(phrase)
-    all_letters = [char for char, count in counts.most_common(10)]
+    for char, count in counts.most_common(10):
+        dists[char] = round(count / sum(counts.values()), 2)
+    all_letters = [dist for dist in dists.items()]
     return category, num_entries, all_letters
 
-def print_distributions(paths: List[str]):
+
+def print_char_counts(paths: List[str]):
     all_paths = [count_chars(path) for path in paths]
     sorted_paths = sorted(all_paths, key=lambda tup: tup[1], reverse=True)
     for path in sorted_paths:
-        print(f"Category: {path[0]} ({path[1]} data points)\n{path[2]}")
+        print(f"Category: {path[0]} ({path[1]} data points)\n{path[2]}\n")
+    print()
 
 
-print_distributions(categories)
+print_char_counts(categories)
